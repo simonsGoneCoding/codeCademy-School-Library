063815d29e1439cbc5f94a543406a695971e38bd
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-04-19 19:36:35 +0100

    Added School class.

--- app.js
+++ app.js
@@ -4,4 +4,40 @@ class School {
     this._level = level;
     this._numberofStudents = numberofStudents;
   }
+
+  //getters:
+  get name() {
+    return this._name;
+  }
+
+  get level() {
+    return this._level;
+  }
+
+  get numberofStudents() {
+    return this._numberofStudents;
+  }
+
+  //setters:
+  set numberofStudents(students) {
+    if (typeof students === "number") {
+      this._numberofStudents = students;
+    } else {
+      console.log("Wrong input!");
+    }
+  }
+
+  //methods:
+  quickFacts() {
+    console.log(
+      `${this.name} educates ${this.numberofStudents} students at the ${this.level} school level.`
+    );
+  }
+
+  static pickSubstituteTeacher(substituteTeachers) {
+    const randomIndexNumber = Math.floor(
+      Math.random() * (substituteTeachers.length - 1)
+    );
+    return substituteTeachers[randomIndexNumber];
+  }
 }
